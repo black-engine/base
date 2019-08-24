93578aec4080b2810dee7b4ef2f9df68a6698e57
Jorge Garza <jorge.garza@gofarma.com>
2019-08-23 22:10:03 -0500

    +Bcrypt methods

--- handlers/api_response_builder.go
+++ handlers/api_response_builder.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Error struct {
@@ -13,6 +14,15 @@ var InvalidTokenError = Error{Message: "Invalid or non existing token", Type: "I
 var UnbindableError = Error{Message: "Unable to bind request", Type: "UNBINDABLE"}
 var WrongCredentialsError = Error{Message: "Wrong credentials", Type: "WRONG_CREDENTIALS"}
 
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	return string(bytes), err
+}
+
+func CheckPassword( storedHash string , passwordCandidate string ) bool {
+	err := bcrypt.CompareHashAndPassword( []byte( storedHash ) , []byte( passwordCandidate ) )
+	return err == nil
+}
 
 func BuildOkApiResponse( context *gin.Context , value interface{} ) {
 	context.JSON( 200 , gin.H{
