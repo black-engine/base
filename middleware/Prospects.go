80601ce0ba311d02a08ed4163441f8e3e925d8da
Jorge Garza <jorge.garza@gofarma.com>
2019-11-12 23:22:19 -0600

    fix in prospect cookie logic

--- middleware/Prospects.go
+++ middleware/Prospects.go
@@ -14,7 +14,7 @@ import (
 
 func Prospects( db *gorm.DB ) gin.HandlerFunc{
 	return func(context *gin.Context) {
-		if p , err := context.Cookie( "p" ); err != nil && len( p ) == 36 {
+		if p , err := context.Cookie( "p" ); err == nil && len( p ) == 36 {
 			context.Set( "p" , p )
 			return //it has prospect
 		}
@@ -24,7 +24,7 @@ func Prospects( db *gorm.DB ) gin.HandlerFunc{
 		prospect.Created = time.Now()
 		prospect.Updated = prospect.Created
 
-		if c , err := context.Cookie( "c" ); err != nil && len( c ) == 36 {
+		if c , err := context.Cookie( "c" ); err == nil && len( c ) == 36 {
 			prospect.CampaignID = &c
 		}
 
@@ -52,7 +52,7 @@ func Prospects( db *gorm.DB ) gin.HandlerFunc{
 			prospect.Language = prospect.Language[0:5]
 		}
 
-		context.SetCookie( "p" , prospect.ID , 2419200 , "/" , helpers.GetCookieDomainFromContext( context ) , false , false )
+		context.SetCookie( "p" , prospect.ID , 30*24*60*60 , "/" , helpers.GetCookieDomainFromContext( context ) , false , false )
 		context.Set( "p" , prospect.ID )
 
 		go func(){
